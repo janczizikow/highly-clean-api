e072c8d71a151fefabb228ce3b3247c13a88e284
Orest Tarasiuk <orest@tarasiuk.me>
2019-10-28 16:54:19 +0100

    resolvers: add relations to queries; user: add [apartments] field

---
+++ src/resolvers/Relations.ts
@@ -0,0 +1,25 @@
+import { Context } from "../utils";
+
+const Relations = {
+
+  User: {
+    apartments(parent, args, ctx: Context) {
+      return ctx.prisma.user({ id: parent.id }).apartments();
+    },
+  },
+
+  Apartment: {
+    places(parent, args, ctx: Context) {
+      return ctx.prisma.apartment({ id: parent.id }).places();
+    }
+  },
+
+  Place: {
+    apartment(parent, args, ctx: Context) {
+      return ctx.prisma.place({ id: parent.id }).apartment();
+    }
+  }
+
+}
+
+export default Relations;
--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,15 +1,17 @@
 import { Query } from "./Query";
+import Relations from "./Relations";
 import { auth } from "./Mutation/auth";
 import { apartment } from "./Mutation/apartment";
 import { place } from "./Mutation/place";
 
 const resolvers = {
   Query,
+  ...Relations,
   Mutation: {
     ...auth,
     ...apartment,
     ...place
-  }
+  },
 };
 
 export default resolvers;
